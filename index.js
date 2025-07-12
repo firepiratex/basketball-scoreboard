ae33be5ffdb428844a32673893385f6880b5a870
wei yu <wyxinzhao@gmail.com>
2025-07-12 14:58:27 +0800

    added localStorage to store points

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
-let homePoint = 0
-let guestPoint = 0
+let homePoint = JSON.parse(localStorage.getItem("home"))
+let guestPoint = JSON.parse(localStorage.getItem("guest"))
 
 let homeEl = document.getElementById('home-point')
 let guestEl = document.getElementById('guest-point')
 
+homeEl.textContent = homePoint
+guestEl.textContent = guestPoint
+
+function storeLocalStorage(club, points) {
+    if (club === "home") {
+        let points = Number(homePoint)
+    } else {
+        let points = Number(guestPoint)
+    }
+    localStorage.setItem(club, JSON.stringify(points))
+}
+
 function add1Home() {
+    homePoint = JSON.parse(localStorage.getItem("home"))
     homePoint += 1
     homeEl.textContent = homePoint
+    storeLocalStorage("home", homePoint)
 }
 
 function add2Home() {
+    homePoint = JSON.parse(localStorage.getItem("home"))
     homePoint += 2
     homeEl.textContent = homePoint
+    storeLocalStorage("home", homePoint)
 }
 
 function add3Home() {
+    homePoint = JSON.parse(localStorage.getItem("home"))
     homePoint += 3
     homeEl.textContent = homePoint
+    storeLocalStorage("home", homePoint)
 }
 
 function add1Guest() {
+    guestPoint = JSON.parse(localStorage.getItem("guest"))
     guestPoint += 1
     guestEl.textContent = guestPoint
+    storeLocalStorage("guest", guestPoint)
 }
 
 function add2Guest() {
+    guestPoint = JSON.parse(localStorage.getItem("guest"))
     guestPoint += 2
     guestEl.textContent = guestPoint
+    storeLocalStorage("guest", guestPoint)
 }
 
 function add3Guest() {
+    guestPoint = JSON.parse(localStorage.getItem("guest"))
     guestPoint += 3
     guestEl.textContent = guestPoint
+    storeLocalStorage("guest", guestPoint)
 }
 
 function newGame() {
-    homePoint = 0
-    guestPoint = 0
-    homeEl.textContent = homePoint
-    guestEl.textContent = guestPoint
+    storeLocalStorage("home", 0)
+    storeLocalStorage("guest", 0)
+    homeEl.textContent = 0
+    guestEl.textContent = 0
 }
